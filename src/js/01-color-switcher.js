018a8edd037b9ac7f8cb3cc43c83a3ddd5bc39a7
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-03-15 01:00:06 +0200

    first homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]'); 
+
+console.log(startBtn);
+console.log(stopBtn);
+
+let timerId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener('click', () => {
+    timerId = setInterval(() => {
+     document.body.style.backgroundColor = getRandomHexColor();
+     }, 1000);
+     startBtn.setAttribute('disabled', true)
+     stopBtn.removeAttribute('disabled') 
+    console.log('click')
+
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(timerId);
+    console.log('click')
+    startBtn.removeAttribute('disabled')
+    stopBtn.setAttribute('disabled', true)
+});
+
