107a291cea1c55e2e112b5dce42975fe408cb805
Sergiy-Shevchenko <sergiy_shevchenko@i.ua>
2023-03-19 08:40:01 +0200

    2+3 homework

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -62,10 +62,10 @@ function overrwriteTime ({ days, hours, minutes, seconds }) {
   refs.seconds.textContent = `${seconds}`;
 };
 
-function pad(value) {
+function addLeadingZero(value) {
   return String(value).padStart(2, '0')
 }
-
+addLeadingZero
 function convertMs(ms) {
     // Number of milliseconds per unit of time
     const second = 1000;
@@ -74,13 +74,13 @@ function convertMs(ms) {
     const day = hour * 24;
 
     // Remaining days
-    const days = pad(Math.floor(ms / day));
+    const days = addLeadingZero(Math.floor(ms / day));
     // Remaining hours
-    const hours = pad(Math.floor((ms % day) / hour));
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
     // Remaining minutes
-    const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
     // Remaining seconds
-    const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
   
     return { days, hours, minutes, seconds };
   } 
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+console.log(form);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event){
+  event.preventDefault();
+
+  let delay = Number(form.delay.value);
+  let step = Number(form.step.value)
+  let amount = Number(form.amount.value)
+
+  for(let i = 1; i <= amount; i += 1) {
+  createPromise(i, delay)
+  .then(({ position, delay }) => {
+    Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`Rejected promise ${position} in ${delay}ms`)
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+  delay += step;
+  }
+};
+
+
+
+
+
+
